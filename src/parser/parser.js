9e4eeade07e9a755097f998b7062d330f8da72ef
Do-Hoon Kim <k99hoon@gmail.com>
2023-08-20 14:26:34 +0900

    parser.js를 수정했고, slack.js를 고쳤습니다.

--- src/parser/parser.js
+++ src/parser/parser.js
@@ -1,3 +1,22 @@
-export const parser = () => {
-    
+import { readFileSync } from 'fs'
+// import path from 'path'
+
+
+export const parser = (fileName) => {
+    // const jsonFilePath = path.join('./', fileName);
+    const jsonFile = readFileSync(fileName, 'utf8');
+
+    try {
+        const jsonData = JSON.parse(jsonFile);
+        processJsonData(jsonData);
+    } catch (parseError) {
+        console.error('JSON 파싱 중 오류가 발생했습니다:', parseError);
+    }
+}
+
+function processJsonData(data) {
+    data.forEach(entry => {
+        const { host, method, status } = entry;
+        console.log(`Host: ${host}, Method: ${method}, Status: ${status}`);
+    });
 }
