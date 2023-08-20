cfa8e19a3d0a8d4c99bd1cde0a1900162f39f9a3
Do-Hoon Kim <k99hoon@gmail.com>
2023-08-20 20:19:53 +0900

    parser.js를 수정했습니다.

--- src/parser/parser.js
+++ src/parser/parser.js
@@ -1,7 +1,6 @@
 import { readFileSync } from 'fs'
 // import path from 'path'
 
-
 export const parser = (fileName) => {
     // const jsonFilePath = path.join('./', fileName);
     const jsonFile = readFileSync(fileName, 'utf8');
@@ -15,8 +14,18 @@ export const parser = (fileName) => {
 }
 
 function processJsonData(data) {
+    const return_data = [];
+
     data.forEach(entry => {
-        const { host, method, status } = entry;
-        console.log(`Host: ${host}, Method: ${method}, Status: ${status}`);
+        const { host, method, status, body } = entry;
+        let id = null, pw = null;
+        if (body !== undefined){
+            id = body.id;
+            pw = body.password;
+        }
+        //console.log(`Host: ${host}, Method: ${method}, Status: ${status}, Body: ${body}, ID: ${id}, PW: ${pw}`);
+        return_data.push([host, method, status, id, pw]);
     });
+
+    return return_data;
 }
