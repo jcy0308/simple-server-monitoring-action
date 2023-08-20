5a86f39de21862833efd6bce55c2a392779a4384
Do-Hoon Kim <k99hoon@gmail.com>
2023-08-20 13:16:18 +0900

    slack.js를 fetch를 사용한 코드로 수정했습니다.

--- src/slack/slack.js
+++ src/slack/slack.js
@@ -1,25 +1,30 @@
-import axios from 'axios';
+const fetch = require('node-fetch');
 
 export const slackAlarm = async (host, slackWebHook) => {
     try {
-        // 호스트에서 알람을 가져오는 로직 구현
-        const alarmData = await fetchAlarmDataFromHost(host);
-
         // Slack에 보낼 메시지 생성
+        const alarmData = await fetchAlarmDataFromHost(host);
         const message = `주의: ${host}에서 알람 발생\n내용: ${alarmData}`;
 
-        // Slack 웹훅으로 메시지 전송
-        const response = await axios.post(slackWebHook, { text: message });
+        const response = await fetch(slackWebHook, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                text: message
+            })
+        });
 
-        if (response.status === 200) {
-            console.log('Slack 알림이 전송되었습니다.');
+        if (response.ok) {
+            console.log('Slack 메시지가 전송되었습니다.');
         } else {
-            console.error('Slack 알림 전송 중 오류가 발생했습니다.');
+            console.error('Slack 메시지 전송 중 오류가 발생했습니다.');
         }
     } catch (error) {
-        console.error('알람 처리 중 오류가 발생했습니다:', error);
+        console.error('Slack 메시지 전송 중 오류가 발생했습니다:', error);
     }
-};x
+};
 
 // 호스트에서 알람 데이터를 가져오는 가정한 함수
 const fetchAlarmDataFromHost = async (host) => {
